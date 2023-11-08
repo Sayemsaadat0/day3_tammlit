7f53947243fdbfd72151685a84617ae9c4083332
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-11-08 10:36:14 +0600

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      // colors: {
+      //   'tm-primary-1': '#your-color-hex-1',
+      //   'tm-primary-2': '#your-color-hex-2',
+      // },
+     },
   },
   plugins: [],
 }
